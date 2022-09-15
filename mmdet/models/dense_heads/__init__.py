a693dc4ce936f3549f6466305dc33ba8d2e787fd
main-2983 <nguyen.mai@sun-asterisk.com>
2022-09-15 13:18:04 +0700

    DecoupleHead_v2

--- mmdet/models/dense_heads/__init__.py
+++ mmdet/models/dense_heads/__init__.py
@@ -41,7 +41,7 @@ from .yolact_head import YOLACTHead, YOLACTProtonet, YOLACTSegmHead
 from .yolo_head import YOLOV3Head
 from .yolof_head import YOLOFHead
 from .yolox_head import YOLOXHead
-from .decoupled_head import DecoupledHead
+from .decoupled_head import DecoupledHead, DecoupledHead_v2
 
 __all__ = [
     'AnchorFreeHead', 'AnchorHead', 'GuidedAnchorHead', 'FeatureAdaption',
@@ -56,4 +56,5 @@ __all__ = [
     'SOLOHead', 'DecoupledSOLOHead', 'CenterNetHead', 'YOLOXHead',
     'DecoupledSOLOLightHead', 'LADHead', 'TOODHead', 'MaskFormerHead',
     'Mask2FormerHead', 'SOLOV2Head', 'DDODHead', 'DecoupledHead',
+    'DecoupledHead_v2'
 ]
--- mmdet/models/dense_heads/decoupled_head.py
+++ mmdet/models/dense_heads/decoupled_head.py
@@ -107,3 +107,142 @@ class DecoupledHead(ATSSHead):
 
         centerness = self.atss_centerness(reg_feat)
         return cls_score, bbox_pred, centerness
+
+
+@HEADS.register_module()
+class DecoupledHead_v2(ATSSHead):
+    def __init__(self,
+                 num_classes,
+                 in_channels,
+                 stacked_convs=3,
+                 **kwargs):
+        super(DecoupledHead_v2, self).__init__(
+            num_classes,
+            in_channels,
+            stacked_convs=stacked_convs,
+            **kwargs
+        )
+
+    def _init_layers(self):
+        """Initialize layers of the head."""
+        self.relu = nn.ReLU(inplace=True)
+        self.cls_convs = nn.ModuleList()
+        self.reg_convs = nn.ModuleList()
+
+        for i in range(self.stacked_convs):
+            chn = self.in_channels if i == 0 else self.feat_channels
+            self.cls_convs.append(
+                ConvModule(
+                    chn,
+                    self.feat_channels,
+                    3,
+                    stride=1,
+                    padding=1,
+                    conv_cfg=self.conv_cfg,
+                    norm_cfg=self.norm_cfg))
+            self.reg_convs.append(
+                ConvModule(
+                    chn,
+                    self.feat_channels,
+                    3,
+                    stride=1,
+                    padding=1,
+                    conv_cfg=self.conv_cfg,
+                    norm_cfg=self.norm_cfg))
+
+        pred_pad_size = self.pred_kernel_size // 2
+
+        self.semantic_branch = nn.ModuleList([
+            ConvModule(
+                self.feat_channels,
+                self.feat_channels,
+                3,
+                stride=1,
+                padding=1,
+                conv_cfg=self.conv_cfg,
+                norm_cfg=self.norm_cfg
+            ),
+            ConvModule(
+                self.feat_channels,
+                self.feat_channels,
+                3,
+                stride=1,
+                padding=1,
+                conv_cfg=self.conv_cfg,
+                norm_cfg=self.norm_cfg
+            )
+        ])
+        self.reg_convs.append(
+            ConvModule(
+                self.feat_channels,
+                self.feat_channels,
+                3,
+                stride=1,
+                padding=1,
+                conv_cfg=self.conv_cfg,
+                norm_cfg=self.norm_cfg
+            )
+        )
+
+        self.atss_cls = nn.Conv2d(
+            self.feat_channels,
+            self.num_anchors * self.cls_out_channels,
+            self.pred_kernel_size,
+            padding=pred_pad_size)
+
+        self.center_reg = nn.Conv2d(
+            self.feat_channels,
+            self.num_base_priors * 2,
+            self.pred_kernel_size,
+            padding=pred_pad_size
+        )
+
+        self.wh_reg = nn.Conv2d(
+            self.feat_channels,
+            self.num_base_priors * 2,
+            self.pred_kernel_size,
+            padding=pred_pad_size)
+
+        self.atss_centerness = nn.Conv2d(
+            self.feat_channels * 2,
+            self.num_base_priors * 1,
+            self.pred_kernel_size,
+            padding=pred_pad_size)
+
+        self.scales = nn.ModuleList(
+            [Scale(1.0) for _ in self.prior_generator.strides])
+
+    def forward_single(self, x, scale):
+        """Forward feature of a single scale level.
+
+        Args:
+            x (Tensor): Features of a single scale level.
+            scale (:obj: `mmcv.cnn.Scale`): Learnable scale module to resize
+                the bbox prediction.
+
+        Returns:
+            tuple:
+                cls_score (Tensor): Cls scores for a single scale level
+                    the channels number is num_anchors * num_classes.
+                bbox_pred (Tensor): Box energies / deltas for a single scale
+                    level, the channels number is num_anchors * 4.
+                centerness (Tensor): Centerness for a single scale level, the
+                    channel number is (N, num_anchors * 1, H, W).
+        """
+        semantic_feat = x
+        bounding_feat = x
+        for cls_conv in self.cls_convs:
+            semantic_feat = cls_conv(semantic_feat)
+        for reg_conv in self.reg_convs:
+            bounding_feat = reg_conv(bounding_feat)
+
+        cls_score = self.atss_cls(self.semantic_branch[0](semantic_feat))
+
+        center_pred = scale(self.center_reg(self.semantic_branch[1](semantic_feat))).float()
+        wh_pred = scale(self.wh_reg(bounding_feat)).float()
+        bbox_pred = torch.cat([center_pred, wh_pred], dim=1)
+
+        bbox_feat = torch.cat([self.semantic_branch[1](semantic_feat), bounding_feat], dim=1)
+        centerness = self.atss_centerness(bbox_feat)
+
+        return cls_score, bbox_pred, centerness
