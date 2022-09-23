3b8cba1d5ea7825c57563357f59f9237be3947cc
main-2983 <nguyen.mai@sun-asterisk.com>
2022-09-23 08:18:58 +0700

    Fix bug in Decoupled v4 and add Decoupled v5

--- mmdet/models/dense_heads/__init__.py
+++ mmdet/models/dense_heads/__init__.py
@@ -42,7 +42,7 @@ from .yolo_head import YOLOV3Head
 from .yolof_head import YOLOFHead
 from .yolox_head import YOLOXHead
 from .decoupled_head import DecoupledHead, DecoupledHead_v2, DecoupledHead_v3, \
-    DecoupledHead_v4
+    DecoupledHead_v4, DecoupledHead_v5
 
 __all__ = [
     'AnchorFreeHead', 'AnchorHead', 'GuidedAnchorHead', 'FeatureAdaption',
@@ -57,5 +57,6 @@ __all__ = [
     'SOLOHead', 'DecoupledSOLOHead', 'CenterNetHead', 'YOLOXHead',
     'DecoupledSOLOLightHead', 'LADHead', 'TOODHead', 'MaskFormerHead',
     'Mask2FormerHead', 'SOLOV2Head', 'DDODHead', 'DecoupledHead',
-    'DecoupledHead_v2', 'DecoupledHead_v3', 'DecoupledHead_v4'
+    'DecoupledHead_v2', 'DecoupledHead_v3', 'DecoupledHead_v4',
+    'DecoupledHead_v5'
 ]
