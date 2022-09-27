2a25c570b8578ca2222a0ab7fa8b9f87a62406bc
main-2983 <nguyen.mai@sun-asterisk.com>
2022-09-27 22:20:04 +0700

    init for v10

--- mmdet/models/dense_heads/__init__.py
+++ mmdet/models/dense_heads/__init__.py
@@ -43,7 +43,7 @@ from .yolof_head import YOLOFHead
 from .yolox_head import YOLOXHead
 from .decoupled_head import DecoupledHead, DecoupledHead_v2, DecoupledHead_v3, \
     DecoupledHead_v4, DecoupledHead_v5, DecoupledHead_v6, DecoupledHead_v7, \
-    DecoupledHead_v8, DecoupledHead_v9
+    DecoupledHead_v8, DecoupledHead_v9, DecoupledHead_v10
 
 __all__ = [
     'AnchorFreeHead', 'AnchorHead', 'GuidedAnchorHead', 'FeatureAdaption',
@@ -60,5 +60,5 @@ __all__ = [
     'Mask2FormerHead', 'SOLOV2Head', 'DDODHead', 'DecoupledHead',
     'DecoupledHead_v2', 'DecoupledHead_v3', 'DecoupledHead_v4',
     'DecoupledHead_v5', 'DecoupledHead_v6', 'DecoupledHead_v7',
-    'DecoupledHead_v8', 'DecoupledHead_v9'
+    'DecoupledHead_v8', 'DecoupledHead_v9', 'DecoupledHead_v10'
 ]
