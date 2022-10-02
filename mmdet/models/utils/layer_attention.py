da0275ec584e6879f0977af9a25e556dfcac1a08
main-2983 <nguyen.mai@sun-asterisk.com>
2022-10-02 21:16:13 +0700

    Layer Attn

---
+++ mmdet/models/utils/layer_attention.py
@@ -0,0 +1,46 @@
+import torch.nn as nn
+
+from mmcv.cnn import ConvModule
+
+
+class LayerAttn(nn.Module):
+    def __init__(self,
+                 channels,
+                 groups,
+                 ratio=8,
+                 act_cfg=dict(
+                     type='Sigmoid'
+                 )):
+        super(LayerAttn, self).__init__()
+        self.channels = channels
+        self.groups = groups
+        self.layer_attn = nn.Sequential(
+            ConvModule(
+                in_channels=channels,
+                out_channels=channels // ratio,
+                kernel_size=1
+            ),
+            ConvModule(
+                in_channels=channels // ratio,
+                out_channels=groups,
+                kernel_size=1,
+                act_cfg=act_cfg
+            )
+        )
+
+    def forward(self, x):
+        b, c, h, w = x.shape
+
+        avg_feat = nn.AdaptiveAvgPool2d(1)(x)
+        weight = self.layer_attn(avg_feat)
+
+        x = x.view(b, self.groups, c // self.groups, 1, 1)
+        weight = weight.view(b, self.groups, 1, 1, 1)
+        _x = x.clone()
+
+        for group in range(self.groups):
+            _x[:, group] = x[:, group] * weight[:, group]
+
+        _x = _x.view(b, c, h, w)
+
+        return _x
